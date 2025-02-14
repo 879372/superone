becc3cfbcaf94133090ed1971d017eb695d77a60
Jose Kaio <kaios5028@gmail.com>
2025-02-14 01:46:44 -0300

    finalizacao do menu suspenso

--- src/components/dropdow-menu/page.tsx
+++ src/components/dropdow-menu/page.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/menubar"
 
 import { useState } from "react"
+import CardWithCircle from "../ui/cardwithcircle";
 
 const infoMenu = {
     alldepartments: [
@@ -91,9 +92,33 @@ const infoMenu = {
                 "Lubrificantes e Óleos",
                 "Segurança Veicular"
             ]
+        },
+        {
+            categories: "Automotivo",
+            subcategories: [
+                "Som Automotivo",
+                "Peças e Acessórios",
+                "Ferramentas",
+                "Lubrificantes e Óleos",
+                "Segurança Veicular"
+            ]
         }
     ]
 };
+
+const CardsDepartaments = [
+    { imageSrc: '/icon1.png', description: 'Arte Papelaria e Armarinhos' },
+    { imageSrc: '/icon2.png', description: 'Saúde' },
+    { imageSrc: '/icon3.png', description: 'Calçados, Roupas e Bolsas' },
+    { imageSrc: '/icon4.png', description: 'Bêbes' },
+    { imageSrc: '/icon5.png', description: 'Casa, Móveis e Decoração' },
+    { imageSrc: '/icon6.png', description: 'Esports e Fitness' },
+    { imageSrc: '/icon7.png', description: 'Beleza e Cuidado Pessoal' },
+    { imageSrc: '/icon8.png', description: 'Imóveis' },
+    { imageSrc: '/icon9.png', description: 'Óculos' },
+    { imageSrc: '/icon10.png', description: 'Serviços' },
+  ];
+  
 
 
 
@@ -114,89 +139,88 @@ export default function DropdowMenu() {
 
 
     return (
-        <div className="flex justify-center items-center w-full ">
-            <div className="flex justify-center items-center w-[70%]">
-                <div className="flex justify-between w-full items-center min-h-[60px]">
-                    <div className="flex gap-5 ">
-                        <div className="">
-                            <Menubar className="w-full">
-                                <MenubarMenu>
-                                    <MenubarTrigger className="h-full rounded-full px-0 ">
-                                        <div
-                                            onClick={toggleAccordion}
-                                            className="flex items-center gap-2 w-full">
-                                            <svg width="23" height="17" viewBox="0 0 23 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M2 14.9998H21" stroke="#F47920" strokeWidth="2.8" strokeLinecap="round" strokeLinejoin="round" />
-                                                <path d="M2 8.5H21" stroke="#F47920" strokeWidth="2.8" strokeLinecap="round" strokeLinejoin="round" />
-                                                <path d="M2 2H21" stroke="#F47920" strokeWidth="2.8" strokeLinecap="round" strokeLinejoin="round" />
-                                            </svg>
-                                            <p className="font-semibold">Departamentos</p>
-                                            <svg
-                                                width="13"
-                                                height="7"
-                                                viewBox="0 0 13 7"
-                                                fill="none"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className={`${isOpen ? "transform rotate-180" : ""} transition-transform duration-300`}
-                                            >
-                                                <path d="M6.57449 4.91758C6.62881 4.83932 6.66561 4.76052 6.72635 4.70361C8.27469 3.24852 9.82653 1.7967 11.3766 0.342704C11.64 0.0959021 11.9478 -0.00424184 12.3117 0.0893351C12.6656 0.180723 12.8888 0.401259 12.9729 0.733429C13.0575 1.06834 12.9396 1.34797 12.6832 1.58766C11.8001 2.41125 10.9205 3.23757 10.0397 4.06335C9.128 4.91758 8.21628 5.77236 7.30398 6.62659C6.77483 7.12183 6.27429 7.12402 5.75097 6.6337C3.95265 4.94932 2.15551 3.26384 0.356023 1.58055C0.170293 1.40707 0.0254464 1.21937 0.00383625 0.964908C-0.0271188 0.592789 0.158027 0.266091 0.499118 0.099732C0.842544 -0.0677214 1.26365 -0.0239424 1.55685 0.228332C1.79806 0.436281 2.02409 0.659552 2.25538 0.876804C3.61974 2.15678 4.98352 3.43731 6.3473 4.71729C6.40278 4.76928 6.46119 4.81798 6.57449 4.91758Z" fill="#F47920" />
-                                            </svg>
-                                        </div>
-                                    </MenubarTrigger>
-                                    <MenubarContent className=" overflow-auto max-h-[550px]">
-                                        <div className="grid grid-cols-4 w-full px-20 py-10 gap-4 text-lg">
-                                            {infoMenu.departments.map((item, index) => (
-                                                <ul key={index}>
-                                                    <li className="font-semibold">{item.categories}</li>
-                                                    {item.subcategories
-                                                        .slice(0, expandedColumns[index] ? item.subcategories.length : 3)
-                                                        .map((subcategory, subIndex) => (
-                                                            <li className="text-[#737373] pl-2 cursor-pointer hover:opacity-80" key={subIndex}>
-                                                                {subcategory}
-                                                            </li>
-                                                        ))}
-                                                    {item.subcategories.length > 3 && (
-                                                        <button onClick={() => handleShowMore(index)} className="mt-2">
-                                                            {expandedColumns[index] ? (
-                                                                <span className="flex gap-1 items-center text-sm">
-                                                                    Ver menos
-                                                                    <svg width="13" height="7" viewBox="0 0 13 7" fill="none"
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        className="transform rotate-180 transition-transform duration-300">
-                                                                        <path d="M6.57449 4.91758C6.62881 4.83932 6.66561 4.76052 6.72635 4.70361C8.27469 3.24852 9.82653 1.7967 11.3766 0.342704C11.64 0.0959021 11.9478 -0.00424184 12.3117 0.0893351C12.6656 0.180723 12.8888 0.401259 12.9729 0.733429C13.0575 1.06834 12.9396 1.34797 12.6832 1.58766C11.8001 2.41125 10.9205 3.23757 10.0397 4.06335C9.128 4.91758 8.21628 5.77236 7.30398 6.62659C6.77483 7.12183 6.27429 7.12402 5.75097 6.6337C3.95265 4.94932 2.15551 3.26384 0.356023 1.58055C0.170293 1.40707 0.0254464 1.21937 0.00383625 0.964908C-0.0271188 0.592789 0.158027 0.266091 0.499118 0.099732C0.842544 -0.0677214 1.26365 -0.0239424 1.55685 0.228332C1.79806 0.436281 2.02409 0.659552 2.25538 0.876804C3.61974 2.15678 4.98352 3.43731 6.3473 4.71729C6.40278 4.76928 6.46119 4.81798 6.57449 4.91758Z" fill="#F47920" />
-                                                                    </svg>
-                                                                </span>
-                                                            ) : (
-                                                                <span className="flex gap-1 items-center text-sm">
-                                                                    Ver mais
-                                                                    <svg width="13" height="7" viewBox="0 0 13 7" fill="none"
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        className="transition-transform duration-300">
-                                                                        <path d="M6.57449 4.91758C6.62881 4.83932 6.66561 4.76052 6.72635 4.70361C8.27469 3.24852 9.82653 1.7967 11.3766 0.342704C11.64 0.0959021 11.9478 -0.00424184 12.3117 0.0893351C12.6656 0.180723 12.8888 0.401259 12.9729 0.733429C13.0575 1.06834 12.9396 1.34797 12.6832 1.58766C11.8001 2.41125 10.9205 3.23757 10.0397 4.06335C9.128 4.91758 8.21628 5.77236 7.30398 6.62659C6.77483 7.12183 6.27429 7.12402 5.75097 6.6337C3.95265 4.94932 2.15551 3.26384 0.356023 1.58055C0.170293 1.40707 0.0254464 1.21937 0.00383625 0.964908C-0.0271188 0.592789 0.158027 0.266091 0.499118 0.099732C0.842544 -0.0677214 1.26365 -0.0239424 1.55685 0.228332C1.79806 0.436281 2.02409 0.659552 2.25538 0.876804C3.61974 2.15678 4.98352 3.43731 6.3473 4.71729C6.40278 4.76928 6.46119 4.81798 6.57449 4.91758Z" fill="#F47920" />
-                                                                    </svg>
-                                                                </span>
-                                                            )}
-                                                        </button>
-                                                    )}
-                                                </ul>
-                                            ))}
-                                        </div>
-                                        <div className="bg=[#FAF7F4] h-[218px] p-[40px]">
-
-                                        </div>
-                                    </MenubarContent>
-                                </MenubarMenu>
-                            </Menubar>
-
-                        </div>
-                        <span className="h-6 w-[1px] bg-[#ABABAB]"></span>
-                        <div>
-                            <p>Descontos Cliente Unimed Natal</p>
-                        </div>
+        <div className="flex justify-center items-center mx-auto w-full bg-[#F7F3EE]">
+            <div className="flex justify-center items-center w-full max-w-[1290px]">
+                <div className="flex gap-3 items-center w-full min-h-[60px]">
+                    <div className="flex gap-5">
+                        <Menubar className="w-full">
+                            <MenubarMenu>
+                                <MenubarTrigger className="h-full rounded-full px-0 ">
+                                    <div
+                                        onClick={toggleAccordion}
+                                        className="flex items-center gap-2 w-full">
+                                        <svg width="23" height="17" viewBox="0 0 23 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2 14.9998H21" stroke="#F47920" strokeWidth="2.8" strokeLinecap="round" strokeLinejoin="round" />
+                                            <path d="M2 8.5H21" stroke="#F47920" strokeWidth="2.8" strokeLinecap="round" strokeLinejoin="round" />
+                                            <path d="M2 2H21" stroke="#F47920" strokeWidth="2.8" strokeLinecap="round" strokeLinejoin="round" />
+                                        </svg>
+                                        <p className="font-semibold">Departamentos</p>
+                                        <svg
+                                            width="13"
+                                            height="7"
+                                            viewBox="0 0 13 7"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className={`${isOpen ? "transform rotate-180" : ""} transition-transform duration-300`}
+                                        >
+                                            <path d="M6.57449 4.91758C6.62881 4.83932 6.66561 4.76052 6.72635 4.70361C8.27469 3.24852 9.82653 1.7967 11.3766 0.342704C11.64 0.0959021 11.9478 -0.00424184 12.3117 0.0893351C12.6656 0.180723 12.8888 0.401259 12.9729 0.733429C13.0575 1.06834 12.9396 1.34797 12.6832 1.58766C11.8001 2.41125 10.9205 3.23757 10.0397 4.06335C9.128 4.91758 8.21628 5.77236 7.30398 6.62659C6.77483 7.12183 6.27429 7.12402 5.75097 6.6337C3.95265 4.94932 2.15551 3.26384 0.356023 1.58055C0.170293 1.40707 0.0254464 1.21937 0.00383625 0.964908C-0.0271188 0.592789 0.158027 0.266091 0.499118 0.099732C0.842544 -0.0677214 1.26365 -0.0239424 1.55685 0.228332C1.79806 0.436281 2.02409 0.659552 2.25538 0.876804C3.61974 2.15678 4.98352 3.43731 6.3473 4.71729C6.40278 4.76928 6.46119 4.81798 6.57449 4.91758Z" fill="#F47920" />
+                                        </svg>
+                                    </div>
+                                </MenubarTrigger>
+                                <MenubarContent className="flex flex-col items-center justify-center overflow-auto max-h-[550px]">
+                                    <div className="grid grid-cols-4 justify-center w-full px-20 py-10 gap-5 text-lg">
+                                        {infoMenu.departments.map((item, index) => (
+                                            <ul key={index}>
+                                                <li className="font-semibold">{item.categories}</li>
+                                                {item.subcategories
+                                                    .slice(0, expandedColumns[index] ? item.subcategories.length : 3)
+                                                    .map((subcategory, subIndex) => (
+                                                        <li className="text-[#737373] pl-2 cursor-pointer hover:opacity-80" key={subIndex}>
+                                                            {subcategory}
+                                                        </li>
+                                                    ))}
+                                                {item.subcategories.length > 3 && (
+                                                    <button onClick={() => handleShowMore(index)} className="mt-2">
+                                                        {expandedColumns[index] ? (
+                                                            <span className="flex gap-1 items-center text-sm">
+                                                                Ver menos
+                                                                <svg width="13" height="7" viewBox="0 0 13 7" fill="none"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    className="transform rotate-180 transition-transform duration-300">
+                                                                    <path d="M6.57449 4.91758C6.62881 4.83932 6.66561 4.76052 6.72635 4.70361C8.27469 3.24852 9.82653 1.7967 11.3766 0.342704C11.64 0.0959021 11.9478 -0.00424184 12.3117 0.0893351C12.6656 0.180723 12.8888 0.401259 12.9729 0.733429C13.0575 1.06834 12.9396 1.34797 12.6832 1.58766C11.8001 2.41125 10.9205 3.23757 10.0397 4.06335C9.128 4.91758 8.21628 5.77236 7.30398 6.62659C6.77483 7.12183 6.27429 7.12402 5.75097 6.6337C3.95265 4.94932 2.15551 3.26384 0.356023 1.58055C0.170293 1.40707 0.0254464 1.21937 0.00383625 0.964908C-0.0271188 0.592789 0.158027 0.266091 0.499118 0.099732C0.842544 -0.0677214 1.26365 -0.0239424 1.55685 0.228332C1.79806 0.436281 2.02409 0.659552 2.25538 0.876804C3.61974 2.15678 4.98352 3.43731 6.3473 4.71729C6.40278 4.76928 6.46119 4.81798 6.57449 4.91758Z" fill="#F47920" />
+                                                                </svg>
+                                                            </span>
+                                                        ) : (
+                                                            <span className="flex gap-1 items-center text-sm">
+                                                                Ver mais
+                                                                <svg width="13" height="7" viewBox="0 0 13 7" fill="none"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    className="transition-transform duration-300">
+                                                                    <path d="M6.57449 4.91758C6.62881 4.83932 6.66561 4.76052 6.72635 4.70361C8.27469 3.24852 9.82653 1.7967 11.3766 0.342704C11.64 0.0959021 11.9478 -0.00424184 12.3117 0.0893351C12.6656 0.180723 12.8888 0.401259 12.9729 0.733429C13.0575 1.06834 12.9396 1.34797 12.6832 1.58766C11.8001 2.41125 10.9205 3.23757 10.0397 4.06335C9.128 4.91758 8.21628 5.77236 7.30398 6.62659C6.77483 7.12183 6.27429 7.12402 5.75097 6.6337C3.95265 4.94932 2.15551 3.26384 0.356023 1.58055C0.170293 1.40707 0.0254464 1.21937 0.00383625 0.964908C-0.0271188 0.592789 0.158027 0.266091 0.499118 0.099732C0.842544 -0.0677214 1.26365 -0.0239424 1.55685 0.228332C1.79806 0.436281 2.02409 0.659552 2.25538 0.876804C3.61974 2.15678 4.98352 3.43731 6.3473 4.71729C6.40278 4.76928 6.46119 4.81798 6.57449 4.91758Z" fill="#F47920" />
+                                                                </svg>
+                                                            </span>
+                                                        )}
+                                                    </button>
+                                                )}
+                                            </ul>
+                                        ))}
+                                    </div>
+                                    <div className="flex max-w-[1290px] bg-[#FAF7F4]">
+                                        {CardsDepartaments.map((card, index) => (
+                                            <CardWithCircle key={index} imageSrc={card.imageSrc} description={card.description} />
+                                        ))}
+                                    </div>
+                                </MenubarContent>
+                            </MenubarMenu>
+                        </Menubar>
                     </div>
-                    <div>
-                        <p>Vendas no uniclub</p>
+                    <span className="h-6 w-[1px] bg-[#ABABAB]"></span>
+                    <div >
+                        <p>Descontos Cliente Unimed Natal</p>
                     </div>
+                </div>
+                <div className="flex justify-end w-full">
+                    <p>Vendas no uniclub</p>
                 </div>
             </div>
         </div >
