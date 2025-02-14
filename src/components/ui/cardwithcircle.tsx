9da5aae3761f54b7f1540e4e1f05ad4afa012690
Jose Kaio <kaios5028@gmail.com>
2025-02-14 01:47:15 -0300

    componente dos cardsCircles

--- src/components/ui/cardwithcircle.tsx
+++ src/components/ui/cardwithcircle.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 
-const CardWithCircle = ({ imageSrc, description}: { imageSrc: string, description: string }) => {
+const CardWithCircle = ({ imageSrc, description }: { imageSrc: string; description: string }) => {
   return (
-    <div className="flex flex-col items-center bg-white shadow-lg rounded-2xl p-4 w-64">
-      <div className="w-24 h-24 rounded-full overflow-hidden border-4 border-gray-300">
-        <Image src={imageSrc} alt="Card Image" width={96} height={96} className="object-cover w-full h-full" />
+    <div className="group flex flex-col items-center bg-transparent rounded-2xl p-4 max-w-[150px] cursor-pointer">
+      <div className="flex justify-center items-center w-24 h-24 rounded-full overflow-hidden bg-[#EAE0D5] transition-colors duration-300 group-hover:bg-[#F47920]">
+        <Image
+          src={imageSrc}
+          alt="Card Image"
+          width={50}
+          height={50}
+          className="object-cover transition-all duration-300 group-hover:grayscale group-hover:brightness-[1000%]"
+        />
       </div>
-      <p className="mt-4 text-center text-gray-700 text-sm">{description}</p>
+      <p className="flex flex-wrap mt-4 text-center text-gray-700 text-sm transition-all duration-300 group-hover:font-semibold">
+        {description}
+      </p>
     </div>
   );
 };
