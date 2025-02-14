f916808ff61edf7f13a655959196cf0db13a4355
Jose Kaio <kaios5028@gmail.com>
2025-02-14 01:46:18 -0300

    alinhando carrosel banners

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -4,36 +4,35 @@ import Image from "next/image";
 import { FC } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 interface ButtonGroupProps {
     next: () => void;
     previous: () => void;
-    carouselState: { currentSlide: number; deviceType: string };
+    carouselState: { currentSlide: number };
 }
 
-const ButtonGroup: FC<ButtonGroupProps> = ({ next, previous, carouselState }) => {
-    const { currentSlide } = carouselState;
-
+const ButtonGroup: FC<ButtonGroupProps> = ({ next, previous }) => {
     return (
-        <div className="carousel-button-group" style={{ position: 'absolute', width: '100%', top: '50%', transform: 'translateY(-50%)', zIndex: 10 }}>
-            <button
-                className={`bg-[#F47920] py-8 px-3 rounded-2xl absolute left-10  -translate-x-1/2 ${currentSlide === 0 ? 'disable' : ''}`}
-                onClick={() => previous()}
-            >
-                <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5.89404 1L1.86136 7L5.89404 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-            </button>
-
-            <button
-                onClick={() => next()}
-                className="bg-[#F47920] py-8 px-3 rounded-2xl absolute right-0 transform translate-x-1/2"
-            >
-                <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.10596 1L5.13864 7L1.10596 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-            </button>
-        </div>
+            <div className="absolute inset-0 top-1/3 flex justify-between items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <button
+                    className="bg-[#F47920] py-7 px-3 rounded-2xl shadow-lg absolute left-[-15px] top-1/2 transform -translate-y-1/2"
+                    onClick={previous}
+                >
+                    <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.89404 1L1.86136 7L5.89404 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </button>
+                <button
+                    className="bg-[#F47920] py-7 px-3 rounded-2xl shadow-lg absolute right-[-15px] top-1/2 transform -translate-y-1/2"
+                    onClick={next}
+                >
+                    <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.10596 1L5.13864 7L1.10596 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </button>
+            </div>
     );
 };
 
@@ -42,63 +41,54 @@ const imageCarousel = [
     { image: "/banner2.png" },
 ];
 
-export default function CarouselCards() {
+export default function CarouselBanner() {
     const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 1,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 1,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        },
+        desktop: { breakpoint: { max: 3000, min: 1024 }, items: 1 },
+        tablet: { breakpoint: { max: 1024, min: 464 }, items: 1 },
+        mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
     };
 
     return (
-        <div className="w-full select-none h-[300px] relative z-10 px-[103px]">
-            <div className="">
-                <Carousel
-                    swipeable
-                    draggable
-                    showDots={false}
-                    responsive={responsive}
-                    ssr
-                    infinite
-                    autoPlay
-                    autoPlaySpeed={5000}
-                    keyBoardControl
-                    customTransition="transform 0.5s ease"
-                    transitionDuration={500}
-                    containerClass="carousel-container"
-                    removeArrowOnDeviceType={["tablet", "mobile"]}
-                    dotListClass="custom-dot-list-style"
-                    itemClass="carousel-item-padding-40-px"
-                    arrows={false}
-                    customButtonGroup={
-                        <ButtonGroup
-                            next={() => { }}
-                            previous={() => { }}
-                            carouselState={{ currentSlide: 0, deviceType: "desktop" }}
+        <div className="w-full select-none h-[300px] mt-[182px] py-10  relative group">
+            <Carousel
+                swipeable
+                draggable
+                showDots={false}
+                responsive={responsive}
+                ssr
+                infinite
+                autoPlay
+                autoPlaySpeed={5000}
+                keyBoardControl
+                customTransition="transform 0.5s ease"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+                arrows={false}
+                renderButtonGroupOutside
+                customButtonGroup={<ButtonGroup next={() => { }} previous={() => { }} carouselState={{ currentSlide: 0 }} />}
+            >
+                {imageCarousel.map((item, index) => (
+                    <div key={index} className="carousel-item w-full h-full select-none ">
+                        <LazyLoadImage
+                            src={item.image}
+                            alt={`Banner ${index + 1}`}
+                            placeholder={
+                                <div className='flex justify-center items-center bg-[#212425] w-full sm:h-[258px] h-[200px]'>
+                                    <div className="w-full skeleton h-full bg-[#212425]"></div>
+                                </div>
+                            }
+                            effect="blur"
+                            threshold={200}
+                            width="100%"
+                            height="100%"
+                            className="object-cover h-full w-full select-none"
                         />
-                    }
-                >
-                    {imageCarousel.map((item, index) => (
-                        <div key={index} className="h-full">
-                            <Image
-                                height={300}
-                                width={300}
-                                src={item.image}
-                                className="object-cover pointer-events-none w-full h-[300px]"
-                                alt={`Banner ${index + 1}`}
-                            />
-                        </div>
-                    ))}
-                </Carousel>
-            </div>
+                    </div>
+                ))}
+            </Carousel>
         </div>
     );
 }
