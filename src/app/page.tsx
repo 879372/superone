ea65b5bc14776bfea8eac95ed3f5329132b3662a
Jose Kaio <kaios5028@gmail.com>
2025-02-14 01:46:06 -0300

    adicionando os componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
-import CarouselCards from "@/components/carousel/carousel";
-import DropdowMenu from "@/components/dropdow-menu/page";
+import CarouselBanner from "@/components/carousel/carousel";
+import CarouselCards from "@/components/carouselCards/carousel";
+
 import Header from "@/components/header/page";
 import CardWithCircle from "@/components/ui/cardwithcircle";
 
@@ -9,26 +10,26 @@ const CardsDepartaments = [
   { imageSrc: '/icon2.png', description: 'Saúde' },
   { imageSrc: '/icon3.png', description: 'Calçados, Roupas e Bolsas' },
   { imageSrc: '/icon4.png', description: 'Bêbes' },
-  { imageSrc: '/icon5.png', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-  { imageSrc: '/icon6.png', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-  { imageSrc: '/icon7.png', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-  { imageSrc: '/icon8.png', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-  { imageSrc: '/icon9.png', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-  { imageSrc: '/icon10.png', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-]
+  { imageSrc: '/icon5.png', description: 'Casa, Móveis e Decoração' },
+  { imageSrc: '/icon6.png', description: 'Esports e Fitness' },
+  { imageSrc: '/icon7.png', description: 'Beleza e Cuidado Pessoal' },
+  { imageSrc: '/icon8.png', description: 'Imóveis' },
+  { imageSrc: '/icon9.png', description: 'Óculos' },
+  { imageSrc: '/icon10.png', description: 'Serviços' },
+];
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-[1300px] mx-auto">
       <Header />
-      <main className="">
-        <DropdowMenu />
-        <CarouselCards />
-        <div className="flex gap-2 justify-center items-end">
+      <main className="flex flex-col items-center justify-center w-full">
+        <CarouselBanner />
+        <div className="flex w-full pt-20 overflow-auto">
           {CardsDepartaments.map((card, index) => (
             <CardWithCircle key={index} imageSrc={card.imageSrc} description={card.description} />
           ))}
         </div>
+        <CarouselCards />
       </main>
     </div>
   );
